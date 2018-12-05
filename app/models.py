a98ece7874c06806b0fcd356be559a9dfbeebeff
AndrewClimber <seoclimber@gmail.com>
2018-12-05 17:55:01 +0700

    v.1

--- app/models.py
+++ app/models.py
@@ -1,8 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-from app import db
+from app import db, login
 from flask_login import UserMixin
-from app import login
 from hashlib import md5
 
 
