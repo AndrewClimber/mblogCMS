55187384ea4f44f763fe824c0347aea6f44acf36
AndrewClimber <seoclimber@gmail.com>
2018-12-13 16:23:24 +0700

    4st commit

--- app/models.py
+++ app/models.py
@@ -43,6 +43,18 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 # *****************************************************************************
